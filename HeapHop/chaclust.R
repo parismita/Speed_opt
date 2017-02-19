c5c0487934390c498367bc9cf8f07132434bc129
parismita <parismita@iitg.ernet.in>
2017-02-20 02:37:22 +0530

    Update chaclust.R

--- HeapHop/chaclust.R
+++ HeapHop/chaclust.R
@@ -4,18 +4,16 @@ library("Matrix")
 chaclust<-function(s,h){
   p<- ncol(s)
   # extracting diagonal band
-  low <- -h
+  low <- 1
   high <- h
-  delta <- row(s) - col(s)
+  delta <- col(s) - row(s)
   s[delta < low | delta > high] <- 0
   
   #create sparse mmatrix of dgCMatric class
   A <- as(s, "sparseMatrix")
-  diag(A) <- 1
   x <- t(A)@x
   
   resP <- adjclust:::HeapHop(x, p, h, 1)
-  plot(resP[1,],resP[2,])
   return(resP)
 }
-chaclust(s,5)
+#chaclust(s,5)
