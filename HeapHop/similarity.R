196c0578486e20c389be4f2477ce934bb9a8c402
parismita <parismita@iitg.ernet.in>
2017-02-20 02:35:39 +0530

    Update similarity.R

--- HeapHop/similarity.R
+++ HeapHop/similarity.R
@@ -8,12 +8,8 @@ data <- read.table("/home/parismita/dissimilarity2.txt")
 #converting to numeric list
 n<-as.numeric(unlist(data))
 #converting to dissimilarity matrix
-dmat<-matrix(c(n),nrow = 150,ncol = 150)
-#View(dmat)
-
-# Gaussian Kernel Matrix
-K<-kernelMatrix(kernel = rbfdot(sigma = 0.1) , as.matrix(dmat))
-View(K)
+K<-matrix(c(n),nrow = 150,ncol = 150)
+#View(K)
 
 # algo to calculate similarity matrix s from the equation given where D is delta, K is kernel 
 #Dkk is sum[k,k':1 to n](delta(xk,xk')), Dij is delta(xi,xj), Dik and Dkj is sum[k:1 to n](delta(xi,xk)) and sum[k:1 to n](delta(xk,xj)) resp
@@ -32,13 +28,14 @@ for (i in 1:n) {
 
 # the similarity matrix
 s <- matrix(s , nrow = 150,ncol = 150)
+image(s)
+p<-150
+h <- 5
 
-
-l <- 5
 # extracting diagonal band
-low <- -l
-high <- l
-delta <- row(s) - col(s)
+low <- 1
+high <- h
+delta <- col(s) - row(s)
 s[delta < low | delta > high] <- 0
 View(s)
 image(s)
@@ -46,10 +43,24 @@ image(s)
 A <- as(s, "sparseMatrix")
 image(A)
 
+
+
 ## some reshaping
-diag(A) <- 1
+#diag(A) <- 1
 x <- t(A)@x
-resP <- adjclust:::HeapHop(x, 150, h, 1)
+
+fit <- adjclust:::adjClustBand_heap(x, p, h, blMin=1)
+plot(fit)
+
+resP <- adjclust:::HeapHop(x, p, h, 1)
 View(resP)
 plot(resP[1,],resP[2,])
 
+#comparision
+diss <- dist(K)
+hc = hclust(d = diss, method = 'ward.D')
+y_hc = cutree(hc,6)
+plot(hc)
+diss=as.matrix(diss)
+clusplot(diss, y_hc, lines = 0, color = TRUE)
+clusplot(s, y_hc, lines = 0, color = TRUE)
