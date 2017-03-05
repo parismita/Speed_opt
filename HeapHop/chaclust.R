002853e44803491cb9f9f9c5566a71b4d2f02f5b
parismita <parismita@iitg.ernet.in>
2017-03-05 20:39:02 +0530

    Update chaclust.R

--- HeapHop/chaclust.R
+++ HeapHop/chaclust.R
@@ -1,10 +1,10 @@
 # this is the function to take similarity matrix as input and give HeapHop clustered object as output
 
-library("adjclust")
-library("Matrix")
+-library("adjclust")
+-library("Matrix")
 
-chaclust<-function(s,h){
-  p<- ncol(s)
+chaclust<-function(s,h,f = 0){
+  p <- ncol(s)
   # extracting diagonal band
   low <- 1
   high <- h
@@ -15,7 +15,13 @@ chaclust<-function(s,h){
   A <- as(s, "sparseMatrix")
   x <- A@x
   
+  if(f==1){
+    fit <- adjclust:::adjClustBand_heap(x,p,h,blMin = 1)
+    return(fit)
+  }
+
   resP <- adjclust:::HeapHop(x, p, h, 1)
   return(resP)
 }
 #chaclust(s,5)
+
