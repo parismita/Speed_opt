19f39d9c0699593363481fa3e06c5a9391a9516c
parismita <parismita@iitg.ernet.in>
2017-03-15 17:56:07 +0530

    Update chclust_usecase.R

--- rioja_easy/chclust_usecase.R
+++ rioja_easy/chclust_usecase.R
@@ -1,7 +1,7 @@
 #This is a example to obtain constrained HAC using dataset extracted from dissimilarity.txt
 
 #reading dataset from txt file
-data <- read.table("/home/parismita/dissimilarity.txt")
+data <- read.table("/home/parismita/cHAC/dissimilarity.txt")
 #converting to numeric list
 n<-as.numeric(unlist(data))
 #converting to dissimilarity matrix
@@ -13,10 +13,8 @@ library(cluster)
 
 #as it is not euclidian dissimilarity matrix we have to find distance matrix by other methods
 
-#case 1
 #diss is computed distance matrix of dissimilarity matrix power of p >1
 diss <- dist(dmat, method='canberra')
-#diss <- dist(dmat, method='minkowski',p = 1)
 #chclust function for Constrained hierarchical clustering method, using coniss method
 clust <- chclust(diss)
 
@@ -24,9 +22,18 @@ clust <- chclust(diss)
 bstick(clust, ng = 10)
 plot(clust, hang=-1 )
 
+
+#chclust function for Constrained hierarchical clustering method, using conslink method
+clust <- chclust(diss, method = "conslink")
+
+#plotting variances of ordination axes/components
+plot(clust, hang=-1 )
+
+
 hc = hclust(d = diss, method = 'ward.D')
 y_hc = cutree(hc,6)
 diss=as.matrix(diss)
 
-clusplot(diss, y_hc, lines = 0, color = TRUE)
+#clusplot(diss, y_hc, lines = 0, color = TRUE)
+
 
