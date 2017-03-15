41a14b59c7ec19e0a2951381b8357a8c93b3e616
parismita <parismita@iitg.ernet.in>
2017-03-15 18:31:00 +0530

    Update comparision.R

--- HeapHop/comparision.R
+++ HeapHop/comparision.R
@@ -5,30 +5,54 @@ library(rioja)
 
 #rioja hclust object
 diss <- dist(K)
-clust = chclust(diss)
-plot(clust, hang=-1 )
-bstick(clust, ng = 10)
+clust <- chclust(diss)
+plot(clust)
+#bstick(clust, ng = 10)
 #doesn't give gain data
-View(head(cbind(clust$merge , clust$height)))
-
-
-
-#hclust object in R
-hc = hclust(d = diss, method = 'ward.D')
-y_hc = cutree(hc,6)
-plot(hc)
-diss=as.matrix(diss)
-clusplot(diss, y_hc, lines = 0, color = TRUE)
+View(head(cbind(clust$merge ,height = clust$height)))
 
 
 
 # hclust object using adjClustBand_heap function
 # where s is the similatrity matrix
+#case 1
+h<-1
 fit <- chaclust(s,h,f = 1)
 plot(fit)
-View(head(cbind(fit$merge ,fit$gains , clust$height)))
+View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
 
-#Heaphop cluster object, as it is not hclust object hence we can't plot it
-#but we can compare the merge and gain data
-resP <- chaclust(s,h)
-View(head(t(resP)))
+
+# hclust object using adjClustBand_heap function
+# where s is the similatrity matrix
+#case 2
+h<-10
+fit <- chaclust(s,h,f = 1)
+plot(fit)
+View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
+
+
+# hclust object using adjClustBand_heap function
+# where s is the similatrity matrix
+#case 3
+h<-15
+fit <- chaclust(s,h,f = 1)
+plot(fit)
+View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
+
+
+# hclust object using adjClustBand_heap function
+# where s is the similatrity matrix
+#case 4
+h<-25
+fit <- chaclust(s,h,f = 1)
+plot(fit)
+View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
+
+# hclust object using adjClustBand_heap function
+# where s is the similatrity matrix
+#case 5
+h<-45
+fit <- chaclust(s,h,f = 1)
+plot(fit,hand = 0)
+View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
+
