16304013b367fd944f23d09156845222e029352c
parismita <parismita@iitg.ernet.in>
2017-03-21 23:15:39 +0530

    Update similarity.R

--- HeapHop/similarity.R
+++ HeapHop/similarity.R
@@ -15,27 +15,28 @@ K<-matrix(c(n),nrow = 150,ncol = 150)
 #Dkk is sum[k,k':1 to n](delta(xk,xk')^2), Dij is delta(xi,xj)^2,
 #Dik and Dkj is sum[k:1 to n](delta(xi,xk)^2) and sum[k:1 to n](delta(xk,xj)^2) resp
 n <- 150
-Dkk <- sum(K^2)
-s <- c()
-for (i in 1:n) {
-  Dik <- sum(K[i,]^2)
-  for (j in 1:n) {
-    Dkj <- sum(K[,j]^2)
-    Dij <- K[i,j]^2
-    #the equation
-    sij <- -0.5*(Dij - 1/n*(Dik+Dkj) + 1/n^2*Dkk)
-    s <- c(s,sij)
-    }
-}
+K <- K^2
+Dkk <- sum(K)
+Dij <- K
+Dik <- apply(K, 1, sum)
+Dkj <- apply(K, 2, sum)
+Dikkj <- 1/n*outer(Dik,Dkj,FUN = '+')
+Dk <- matrix(1/n^2*Dkk,nrow=150,ncol=150,byrow=TRUE)
 
 # the similarity matrix
-s <- matrix(s , nrow = 150,ncol = 150)
-image(s)
+s <- -0.5*(Dij - Dikkj + Dk)
+
+# scaling of the similarity matrix and making diagonal column as 1,
+# as required by adjClustBand_heap
+s_scaled <- sweep(s,1,sqrt(diag(s)),'/')
+s_scaled <- sweep(s_scaled,2,sqrt(diag(s)),'/')
+
+#image(sp)
 p<-150
 h <- 5
 
 # using function chaclust to get HeapHop object
-resP <- chaclust(s,h)
+resP <- chaclust(s_scaled,h)
 
 
 View(resP)
