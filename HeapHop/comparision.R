0126971b3b2b0cbea1cdd34b34ba38c184a5b6c1
parismita <parismita@iitg.ernet.in>
2017-03-22 00:50:45 +0530

    Update comparision.R

--- HeapHop/comparision.R
+++ HeapHop/comparision.R
@@ -4,7 +4,7 @@ library(rioja)
 
 
 #rioja hclust object
-diss <- dist(K)
+diss <- as.dist(K)
 clust <- chclust(diss)
 plot(clust)
 #bstick(clust, ng = 10)
@@ -14,45 +14,41 @@ View(head(cbind(clust$merge ,height = clust$height)))
 
 
 # hclust object using adjClustBand_heap function
-# where s is the similatrity matrix
+# where s_scaled is the scaled similatrity matrix
 #case 1
-h<-1
-fit <- chaclust(s,h,f = 1)
+h<-5
+fit <- chaclust(s_scaled,h,f = 1)
 plot(fit)
 View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
 
 
 # hclust object using adjClustBand_heap function
-# where s is the similatrity matrix
+# where s_scaled is the similatrity matrix
 #case 2
-h<-10
-fit <- chaclust(s,h,f = 1)
+h<-20
+fit <- chaclust(s_scaled,h,f = 1)
 plot(fit)
 View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
 
 
 # hclust object using adjClustBand_heap function
-# where s is the similatrity matrix
+# where s_scaled is the similatrity matrix
 #case 3
-h<-15
-fit <- chaclust(s,h,f = 1)
+h<- ncol(s_scaled)
+fit <- chaclust(s_scaled,h,f = 1)
 plot(fit)
 View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
 
+#Recalculating the similarity matrix as s_re = 2-2*s' and rechecking the rioja cluster value
+s_re <- 2 - 2*s_scaled
 
-# hclust object using adjClustBand_heap function
-# where s is the similatrity matrix
-#case 4
-h<-25
-fit <- chaclust(s,h,f = 1)
-plot(fit)
-View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
+#rioja hclust object
+diss <- as.dist(s_re)
+clust <- chclust(diss)
+plot(clust)
+bstick(clust, ng = 10)
+#doesn't give gain data
+View(head(cbind(clust$merge ,height = clust$height)))
 
-# hclust object using adjClustBand_heap function
-# where s is the similatrity matrix
-#case 5
-h<-45
-fit <- chaclust(s,h,f = 1)
-plot(fit,hand = 0)
-View(head(cbind(fit$merge ,gain = fit$gains , height = clust$height)))
-
+# we see that the rioja and adjClustBand_heap give same value of clustering
+#when we give scaled matrix s' to adjClustBand_heap and 2-2s' to rioja.
